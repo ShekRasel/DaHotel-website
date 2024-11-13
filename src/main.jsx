932b25c6784062626f291c07ff61b46368ea6fd0
Shek Rasel <128912090+ShekRasel@users.noreply.github.com>
2024-11-13 23:18:19 +0600

    routing setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import About from './pages/About.jsx'
+import Home from './pages/Home.jsx'
+import AppLayout from './common components/AppLayout/AppLayout.jsx'
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout/>,
+    children:[
+      {
+        path:'/',
+        element:<Home/>
+      },
+      {
+        path:'/about',
+        element:<About/>
+      }
+    ]
+  },
+  
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes}/>
   </StrictMode>,
 )
