c0faf96d61a33b3cd36f5081d37cb667baf11146
Shek Rasel <128912090+ShekRasel@users.noreply.github.com>
2024-11-22 16:02:39 +0600

    popup menu work,

--- src/common components/components/Navbar.jsx
+++ src/common components/components/Navbar.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 function Navbar() {
   const [stickyNav, setStickyNav] = useState(false);
   const [hamBurger, setHamburger] = useState(false);
+  const [subMenu, setSubMenu] = useState();
 
   const afterScrollSticky = () => {
     if (window.scrollY >= 250) {
@@ -23,33 +24,66 @@ function Navbar() {
   }, []);
 
   const navlinks = [
-    { link: "Home" , path: '/'},
-    { link: "About" , path:'about'},
-    { link: "Our Rooms" ,path:'ourrooms'},
-    { link: "Pages" ,path:''},
-    { link: "Facilities" ,path:''},
-    { link: "Blog", path:'blog' },
-    { link: "Contact Us" ,path:''},
+    { link: "Home", path: "/" },
+    { link: "About", path: "about" },
+    { link: "Our Rooms", path: "ourrooms" },
+    { link: "Pages" },
+    { link: "Facilities", path: "" },
+    { link: "Blog", path: "" },
+    { link: "Contact Us", path: "" },
   ];
   return (
     <header
       className={` text-white  w-full absolute  items-center top-0   ease-out transition-all duration-500 ${
-        stickyNav ? "sticky top-0 z-50" : 'z-50'
+        stickyNav ? "sticky top-0 z-50" : "z-50"
       }`}
     >
       <div className="px-4 md:px-10 xl:px-20 bg-[#2C4549]">
-        
         <div className="flex justify-between w-full  py-3 items-center  ">
           <div>
             <h1 className="text-xl md:text-3xl lg:text-4xl font-semibold italic">
               Da<span className="font-charm">Hotel</span>
             </h1>
           </div>
-          <div className="lg:flex gap-7 2xl:gap-10 hidden">
+
+          <div className="lg:flex gap-7 2xl:gap-10 hidden relative">
             {navlinks.map((navlink, index) => (
-              <h1 className="text-lg font-firsSans" key={index}>
-               <Link to={navlink.path}>{navlink.link}</Link>
-              </h1>
+              <div
+                className=""
+                key={index}
+                onMouseEnter={() => setSubMenu(index)}
+                onMouseLeave={() => setSubMenu(null)}
+              >
+                <h1 className="text-lg font-firsSans">
+                  <Link to={navlink.path}>{navlink.link}</Link>
+                </h1>
+                {subMenu === 3 && (
+                  <div
+                    className=" absolute  cursor-pointer font-roboto tracking-wider text-xl  text-black left-1/3 "
+                    onMouseEnter={() => setSubMenu(3)}
+                    onMouseLeave={() => setSubMenu(null)}
+                  >
+                    <div className="bg-white rounded-sm py-4 mt-7">
+                      <h1 className="pr-36 px-6 py-4">Gallary</h1>
+                      <h1 className="border-t  px-6 py-4">Our Team</h1>
+                      <h1 className="border-t  px-6 py-4 ">Pricing</h1>
+                      <h1 className="border-t  px-6 py-4">FAQ</h1>
+                    </div>
+                  </div>
+                )}
+                {subMenu === 4 && (
+                  <div
+                    className=" absolute top-full cursor-pointer font-roboto tracking-wider text-xl  text-black left-1/3 "
+                    onMouseEnter={() => setSubMenu(4)} 
+                    onMouseLeave={() => setSubMenu(null)}
+                  >
+                    <div className="bg-white rounded-sm py-4 mt-7">
+                      <h1 className="pr-36 px-6 py-4">Our Services</h1>
+                      <h1 className="border-t  px-6 py-4">Services Details</h1>
+                    </div>
+                  </div>
+                )}
+              </div>
             ))}
           </div>
 
@@ -65,22 +99,23 @@ function Navbar() {
             {hamBurger ? <RxCross2 /> : <FaBars />}
           </div>
         </div>
-
-
       </div>
-      
-        <div className={` lg:hidden transition-all px-4 md:px-10 ease-out duration-500 overflow-hidden text-black ${hamBurger ? 'max-h-96 ': 'max-h-0'}`}>
-          <div className="w-full h-full bg-white p-8 pt-0 pb-0 shadow-lg border-b">
+
+      <div
+        className={` lg:hidden transition-all px-4 md:px-10 ease-out duration-500 overflow-hidden text-black ${
+          hamBurger ? "max-h-96 " : "max-h-0"
+        }`}
+      >
+        <div className="w-full h-full bg-white p-8 pt-0 pb-0 shadow-lg border-b">
           {navlinks.map((navlink, index) => (
-                <div className=" border-t py-2 sm:py-3 "  key={index} >
-                  <h1 className={`text-lg font-firsSans`}>
-                  <Link to={navlink.path}>{navlink.link}</Link>
-                  </h1>
-                </div>
-              ))}
-          </div>
+            <div className=" border-t py-2 sm:py-3 " key={index}>
+              <h1 className={`text-lg font-firsSans`}>
+                <Link to={navlink.path}>{navlink.link}</Link>
+              </h1>
+            </div>
+          ))}
         </div>
-      
+      </div>
     </header>
   );
 }
